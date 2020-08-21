39e34e5f8e2526ac026c0b78999e088e0e0cd358
Syed Ali Uz Zaman <syedaliuzzaman@gmail.com>
2020-08-21 12:51:00 -0700

    Created Complete Card

--- src/App.js
+++ src/App.js
@@ -2,13 +2,23 @@ import React from 'react';
 
 import { Cards, Chart, CountryPicker } from './components';
 import styles from './App.module.css';
+import { fetchData } from './api';
 
 
 class App extends React.Component{
+    state = {
+        data : {},
+    }
+    async componentDidMount(){
+        const fetchedData = await fetchData();
+        // console.log(data);
+        this.setState({data : fetchedData})
+    }
     render(){
+        const { data } = this.state;
         return(
             <div className={styles.container}>
-                <Cards />
+                <Cards data = {data}/>
                 <Chart />
                 <CountryPicker />
             </div>
