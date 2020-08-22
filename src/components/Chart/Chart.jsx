2d27b51be7f6b4dd9a60b06986f7b992cf244001
Syed Ali Uz Zaman <syedaliuzzaman@gmail.com>
2020-08-22 04:22:11 -0700

    Created Succesfully Chart

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -1,9 +1,40 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import {fetchDailyData} from './../../api';
+import { Line, Bar } from 'react-chartjs-2';
+import styles from './Chart.module.css';
+
 
 const Chart = () => {
+    const [dailyData, setDailyData] = useState([])
+
+    useEffect(() => {
+        const fetchApi = async () => {
+        setDailyData(await fetchDailyData());
+    }
+        console.log(dailyData);
+        fetchApi();
+    },[]);
+    const linerChart = (
+        dailyData.length
+        ? (<Line data={{
+            labels : dailyData.map(({date}) => date),
+            datasets: [{
+                data : dailyData.map(({confirmed}) => confirmed),
+                fill: true,
+                borderColor: '#3333ff',
+                label : 'Infected'
+            } , {
+                data : dailyData.map(({deaths}) => deaths),
+                fill: true,
+                borderColor: 'red',
+                label : 'Deaths',
+                backgroundColor: 'rgb(255,0 , 0, 0.5)'
+            }],
+        }}/>) : null
+        );
     return(
-        <div>
-            <h1> Chart </h1>
+        <div className={styles.container}>
+            {linerChart}
         </div>
     )
 }
